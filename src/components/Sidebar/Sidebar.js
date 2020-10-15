c4d2f23ba09efadbeeca4cf55325b5fed1d57f5e
chriserl <chriserldev@gmail.com>
2020-10-15 23:25:33 +0000

    made sidebar closeable

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,56 +1,103 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./sidebar.scss";
 import profileImage from "../../assets/images/jade.jpg";
 
-const Sidebar = () => {
+const UserLinks = () => {
   return (
-    <div className="sidebar">
-      <button className="hide-sidebar icon-font-button">
+    <div className="view-links app-links">
+      <ul className="links">
+        <li className="link-item">
+          <NavLink
+            to="/"
+            className="view-link"
+            activeClassName="active-view-link"
+          >
+            <span className="las la-home icon-regular link-icon"></span>
+            <p className="link-text">Home</p>
+          </NavLink>
+        </li>
+        <li className="link-item">
+          <NavLink
+            to="/"
+            className="view-link"
+            activeClassName="active-view-link"
+          >
+            <span className="las la-broadcast-tower icon-regular link-icon"></span>
+            <p className="link-text">Trending</p>
+          </NavLink>
+        </li>
+        <li className="link-item">
+          <NavLink
+            to="/"
+            className="view-link"
+            activeClassName="active-view-link"
+          >
+            <span className="las la-folder icon-regular link-icon"></span>
+            <p className="link-text">Subscriptions</p>
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const Subscriptions = () => {
+  return (
+    <div className="subscriptions">
+      <div className="channel-image-container">
+        <img src={profileImage} alt="channel" className="channel-image" />
+      </div>
+      <div className="channel-image-container">
+        <img src={profileImage} alt="channel" className="channel-image" />
+      </div>
+      <div className="channel-image-container">
+        <img src={profileImage} alt="channel" className="channel-image" />
+      </div>
+      <div className="channel-image-container">
+        <img src={profileImage} alt="channel" className="channel-image" />
+      </div>
+      <div className="channel-image-container">
+        <img src={profileImage} alt="channel" className="channel-image" />
+      </div>
+      <div className="channel-image-container">
+        <img src={profileImage} alt="channel" className="channel-image" />
+      </div>
+      <div className="channel-image-container">
+        <img src={profileImage} alt="channel" className="channel-image" />
+      </div>
+      <div className="channel-image-container">
+        <img src={profileImage} alt="channel" className="channel-image" />
+      </div>
+      <div className="channel-image-container">
+        <img src={profileImage} alt="channel" className="channel-image" />
+      </div>
+    </div>
+  );
+};
+
+const Sidebar = () => {
+  let [sidebarState, setSidebarState] = useState(() => "sidebar-opened");
+
+  const toggleSidebar = () => {
+    setSidebarState(() =>
+      sidebarState === "sidebar-opened" ? "sidebar-closed" : "side-bar-opened"
+    );
+  };
+
+  return (
+    <div className={sidebarState}>
+      <button
+        onClick={() => toggleSidebar()}
+        className="hide-sidebar icon-font-button"
+      >
         <span className="las la-angle-double-left"></span>
       </button>
       <div className="profile">
         <img src={profileImage} alt="jade" className="profile-image" />
         <p className="profile-name">Unjaded Jade</p>
       </div>
-
-      <hr />
-
-      <div className="view-links app-links">
-        <ul className="links">
-          <li className="link-item">
-            <NavLink
-              to="/"
-              className="view-link"
-              activeClassName="active-view-link"
-            >
-              <span className="las la-home icon-regular link-icon"></span>
-              <p className="link-text">Home</p>
-            </NavLink>
-          </li>
-          <li className="link-item">
-            <NavLink
-              to="/"
-              className="view-link"
-              activeClassName="active-view-link"
-            >
-              <span className="las la-broadcast-tower icon-regular link-icon"></span>
-              <p className="link-text">Trending</p>
-            </NavLink>
-          </li>
-          <li className="link-item">
-            <NavLink
-              to="/"
-              className="view-link"
-              activeClassName="active-view-link"
-            >
-              <span className="las la-folder icon-regular link-icon"></span>
-              <p className="link-text">Subscriptions</p>
-            </NavLink>
-          </li>
-        </ul>
-      </div>
-
+      <UserLinks />
       <hr />
 
       <div className="view-links user-links">
@@ -88,35 +135,7 @@ const Sidebar = () => {
         </ul>
       </div>
 
-      <div className="subscriptions">
-        <div className="channel-image-container">
-          <img src={profileImage} alt="channel" className="channel-image" />
-        </div>
-        <div className="channel-image-container">
-          <img src={profileImage} alt="channel" className="channel-image" />
-        </div>
-        <div className="channel-image-container">
-          <img src={profileImage} alt="channel" className="channel-image" />
-        </div>
-        <div className="channel-image-container">
-          <img src={profileImage} alt="channel" className="channel-image" />
-        </div>
-        <div className="channel-image-container">
-          <img src={profileImage} alt="channel" className="channel-image" />
-        </div>
-        <div className="channel-image-container">
-          <img src={profileImage} alt="channel" className="channel-image" />
-        </div>
-        <div className="channel-image-container">
-          <img src={profileImage} alt="channel" className="channel-image" />
-        </div>
-        <div className="channel-image-container">
-          <img src={profileImage} alt="channel" className="channel-image" />
-        </div>
-        <div className="channel-image-container">
-          <img src={profileImage} alt="channel" className="channel-image" />
-        </div>
-      </div>
+      <Subscriptions />
 
       <hr />
 
