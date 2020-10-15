f0f2dadbb9975e6f9f97466277561f7721852294
chriserl <chriserldev@gmail.com>
2020-10-15 18:37:10 +0000

    setup base sass styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import App from "./pages/app";
+import "./sass/main.scss";
 
 ReactDOM.render(
   <React.StrictMode>
