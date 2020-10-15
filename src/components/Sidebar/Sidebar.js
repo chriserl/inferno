c3280f1c1c2711019444b368584a841c55838c1e
chriserl <chriserldev@gmail.com>
2020-10-15 21:11:33 +0000

    built sidebar and changed fonts

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,41 @@
 import React from "react";
 import "./sidebar.scss";
+import profileImage from "../../assets/images/jade.jpg";
 
 const Sidebar = () => {
-  return <div className="sidebar"></div>;
+  return (
+    <div className="sidebar">
+      <button className="hide-sidebar icon-font-button">
+        <span className="las la-angle-double-left"></span>
+      </button>
+      <div className="profile">
+        <img src={profileImage} alt="jade" className="profile-image" />
+        <p className="profile-name">Unjaded Jade</p>
+      </div>
+      <div className="view-links">
+        <ul className="links">
+          <li className="link-item">
+            <a href="/" className="view-link">
+              <span className="las la-home"></span>
+              Home
+            </a>
+          </li>
+          <li className="link-item">
+            <a href="/" className="view-link">
+              <span className="las la-home"></span>
+              Home
+            </a>
+          </li>
+          <li className="link-item">
+            <a href="/" className="view-link">
+              <span className="las la-home"></span>
+              Home
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
