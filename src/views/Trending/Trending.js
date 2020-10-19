092f0a0495a97a9a5e7b8b88b85aabcbd8796995
chriserl <chriserldev@gmail.com>
2020-10-19 10:36:16 +0000

    created trending component

--- src/views/Trending/Trending.js
+++ src/views/Trending/Trending.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./trending.scss";
+import VideosGrid from "../../components/VideosGrid/VideosGrid";
 
 const Trending = () => {
-  return <div className="trending">Trending</div>;
+	return (
+		<div className="trending">
+			<p className="section-title">Trending</p>
+			<VideosGrid />
+		</div>
+	);
 };
 
 export default Trending;
